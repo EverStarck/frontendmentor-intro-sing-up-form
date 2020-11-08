dc666684575b6f96530adea0a8c963109bf2609c
EverStarck <EverStarckOne@protonmail.com>
2020-11-08 16:17:59 -0600

    animation with gsap

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import styled from "styled-components";
 import Info from "./components/Info";
 import Button from "./components/Button";
